48f6e10d25c194157e06ac2d116d470cafe96d62
Martin Espadas Jr <bobey90@gmail.com>
2025-06-04 16:54:48 -0700

    Add project files.

---
+++ MainCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+
+namespace BNJRevitAddinTemplate
+{
+    [Transaction(TransactionMode.Manual)]
+    public class MainCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            TaskDialog.Show("Hello Revit","BNJ Templates");
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Properties/Resources.Designer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNJRevitAddinTemplate.Properties
+{
+    public partial class Resources : Component
+    {
+        public Resources()
+        {
+            InitializeComponent();
+        }
+
+        public Resources(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
